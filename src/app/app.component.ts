c6303969a21ca17d004e072a27fc4c527c3660f2
bronzo97 <danilobronzo97@hotmail.it>
2023-11-28 22:40:11 +0100

    creating nav and footer components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './navbar/navbar.component';
+import { Footer } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, Navbar, Footer],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.scss',
+})
+
+export class Navbar {
+    linkMenu = [
+        'home', 'menu', 'lavora con noi', 'dove siamo'
+    ]
+}
