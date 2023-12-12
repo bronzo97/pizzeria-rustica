ace2f552a154434cc72dd8639c2b240319341d7e
bronzo97 <danilobronzo97@hotmail.it>
2023-12-13 00:12:28 +0100

    adds router [not working]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar.component';
 import { Footer } from './footer/footer.component';
+import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, Navbar, Footer],
+  imports: [CommonModule, RouterOutlet, Navbar, Footer, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/work-with-us/work-with-us.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-work-with-us',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './work-with-us.component.html',
+  styleUrl: './work-with-us.component.scss'
+})
+export class WorkWithUsComponent {
+
+}
