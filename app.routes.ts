b72efc06e258c9feadad433b9a0c80297b4e317f
bronzo97 <danilobronzo97@hotmail.it>
2024-01-01 23:30:46 +0100

    addes roots (menu, contacts)

--- app.routes.ts
+++ app.routes.ts
@@ -2,11 +2,21 @@ import {Routes} from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './src/app/home/home.component';
 import { WorkWithUsComponent } from './src/app/work-with-us/work-with-us.component';
+import { MenuComponent } from './src/app/menu/menu.component';
+import { ContactsComponent } from './src/app/contacts/contacts.component';
 
 export const routes = [] = [
     {
         path: '/home',
         component: HomeComponent
+    },
+    {
+        path: '/menu',
+        component: MenuComponent
+    },
+    {
+        path: '/contacts',
+        component: ContactsComponent
     },
     {
         path: '/work-with-us',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { WorkWithUsComponent } from './work-with-us/work-with-us.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
+    {path: 'menu', component: MenuComponent},
+    {path: 'contacts', component: ContactsComponent},
     {path: 'work-with-us', component: WorkWithUsComponent},
 ];
